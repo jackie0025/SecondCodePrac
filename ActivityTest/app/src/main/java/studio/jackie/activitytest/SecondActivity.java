d1b2bb0bfdf258bbf10e985e842e34ad98d19580
J.Z <jackie0025@163.com>
2017-08-05 12:48:29 +0800

    intent

---
+++ ActivityTest/app/src/main/java/studio/jackie/activitytest/SecondActivity.java
@@ -0,0 +1,12 @@
+package studio.jackie.activitytest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ HelloWorld/app/src/main/java/studio/jackie/helloworld/MainActivity.java
@@ -0,0 +1,17 @@
+package studio.jackie.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String TAG="myTag";
+        Log.d(TAG, "onCreate: test");
+        Log.e(TAG, "onCreate: LogE");
+}
+}
